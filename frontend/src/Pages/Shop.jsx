43350ec2b2a322c8a3b22625d6eeb617ef100ad0
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-05-20 09:26:01 +0530

    newCollection and offer section

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../Components/Hero/Hero";
 import Popular from "../Components/Popular/Popular";
+import Offers from "../Components/Offer/Offers";
+import NewCollections from "../Components/NewCollections/NewCollections";
 
 const Shop = () => {
 	return (
 		<>
 			<Hero />
-			<Popular/>
+			<Popular />
+			<Offers />
+			<NewCollections />
 		</>
 	);
 };
