b8dd49cf5e2dc4638e534276d2c69d551c865853
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-05-20 09:49:46 +0530

    newsletter and footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Shop from './Pages/Shop';
 import LoginSignup from './Pages/LoginSignup';
 import Product from './Pages/Product';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
@@ -16,13 +17,14 @@ function App() {
 					<Route path="/" element={<Shop />} />
 					<Route path="/mens" element={<ShopCategory category="mens" />} />
 					<Route path="/womens" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kid" />} />
-          <Route path='/product' element={<Product />}>
-            <Route path=':productId' element={<Product/>}/>
-          </Route>
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/login' element={<LoginSignup/>}/>
+					<Route path="/kids" element={<ShopCategory category="kid" />} />
+					<Route path="/product" element={<Product />}>
+						<Route path=":productId" element={<Product />} />
+					</Route>
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/login" element={<LoginSignup />} />
 				</Routes>
+				<Footer />
 			</BrowserRouter>
 		</>
 	);
